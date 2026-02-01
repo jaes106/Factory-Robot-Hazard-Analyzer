95cc19a29e39128361b53e97815d46308d878418
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-02-02 02:10:11 +0530

    [Jason] Added: UC6: Introduce Custom Exception – RobotSafetyException

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -2,14 +2,12 @@ import java.util.Scanner;
 
 public class FactoryRobotHazardAnalyzer {
 
-    static double calculateHazardRisk(double armPrecision, int workerDensity, String machineryState){
+    static double calculateHazardRisk(double armPrecision, int workerDensity, String machineryState) throws RobotSafetyException {
         if (armPrecision < 0.0 || armPrecision > 1.0) {
-            System.out.println("Invalid arm precision");
-            return -1;
+            throw new RobotSafetyException("Error: Arm precision must be 0.0-1.0");
         }
         if (workerDensity < 1 || workerDensity > 20) {
-            System.out.println("Invalid worker density");
-            return -1;
+            throw new RobotSafetyException("Error: Worker density must be 1-20");
         }
 
 
@@ -19,8 +17,7 @@ public class FactoryRobotHazardAnalyzer {
             case "faulty" -> machineRiskFactor = 2.0;
             case "critical" -> machineRiskFactor = 3.0;
             default -> {
-                System.out.println("Invalid machinery state input");
-                return -1;
+                throw new RobotSafetyException("Error: Machinery state should be worn, faulty or critical!");
             }
         };
         return ((1.0 - armPrecision) * 15.0) + (workerDensity * machineRiskFactor);
@@ -39,9 +36,12 @@ public class FactoryRobotHazardAnalyzer {
         System.out.println("Enter Machinery State (Worn/Faulty/Critical):");
         String machineryState = sc.nextLine();
 
-
-        System.out.println("Hazard Risk Factor: " + calculateHazardRisk(armPrecision,workerDensity,machineryState));
-
+        try {
+            System.out.println("Hazard Risk Factor: " + calculateHazardRisk(armPrecision, workerDensity, machineryState));
+        }
+        catch (RobotSafetyException rse){
+            System.out.println(rse.getMessage());
+        }
         sc.close();
     }
 }
